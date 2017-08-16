9bd4e03158b59bb5a585fb5d83fc1f643e3f6315
FAB3659 <firas.abedelnabi@viseo.com>
2017-08-16 17:55:13 +0200

    minor

--- src/main/java/com/viseo/reflection/BaseClass.java
+++ src/main/java/com/viseo/reflection/BaseClass.java
@@ -5,23 +5,23 @@ package main.java.com.viseo.reflection;
  */
 public class BaseClass {
 
-    public int classAttribute;
+    public int baseClassAttribute;
 
     private static void firstMethod(){
         System.out.println("First Class Method");
     }
 
-    public int secondMethod(){
-        System.out.println("Second Class Method");
+    public int secondMethod(int val){
+        System.out.println("Second Class Method : " + val);
         return 0;
     }
 
     public void thirdMethod(){
-        System.out.println("Third Class Method");
+        System.out.println("Third Base Class Method");
     }
 
     private String fourthMethod(){
-        System.out.println("Fourth Class Method");
+        System.out.println("Fourth Base Class Method");
         return "";
     }
 
